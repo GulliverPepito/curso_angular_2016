bab74e6791119ef8e594f21b75b1d17ac66a8352
jmortega <jmoc25@gmail.com>
2016-11-08 20:37:04 +0100

    cursoa-angular-2016

---
+++ sesion1/counter_basic/app.js
@@ -0,0 +1,9 @@
+﻿angular.module("exampleApp", [])
+    .controller("defaultCtrl", function ($scope, $log) {
+
+        $scope.counter = 0;
+
+        $scope.incrementCounter = function () {
+            $scope.counter++;
+        }
+    });
