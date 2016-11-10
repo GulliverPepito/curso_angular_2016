5269bbb869eaa656555e166fd9a7d4e5e882b6c4
jmortega <jmoc25@gmail.com>
2016-11-10 21:20:01 +0100

    curso_angular_2016

--- sesion2/ejercicio_watch/myController.js
+++ sesion2/ejercicio_watch/myController.js
@@ -3,7 +3,6 @@
 	function controller($scope) {
 		
 		$scope.errorPassword =true;
-		$scope.errorPassword2 =false;
 		
 		$scope.$watch('password',function(nuevo,anterior){
 			if(!nuevo) return;
@@ -13,18 +12,6 @@
 				$scope.errorPassword =false;
 			}
 		});
-		
-		$scope.$watchGroup(['password','password2'],function(nuevo,anterior){
-			if(!nuevo) return;
-
-			if(nuevo[0]===nuevo[1]){
-				$scope.errorPassword2 =false;
-			}else{
-				$scope.errorPassword2 =true;
-			}
-		});
-
-
 	}
 	myApp.controller("myController", controller);
 }(angular.module("myApp")));
